e732ae72b50e429eb318931e001b462001e440c2
farhansetyadi <farhan_setyadi@apps.ipb.ac.id>
2020-10-02 07:36:47 +0700

    Pert 5 - Bootstrap, CSS, JS di Codeigniter

--- app/app/Config/Routes.php
+++ app/app/Config/Routes.php
@@ -30,7 +30,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Mahasiswa::ucapan');
+// $routes->get('/', 'Mahasiswa::ucapan');
+$routes->get('/', 'Beranda::index');
 
 /**
  * --------------------------------------------------------------------
