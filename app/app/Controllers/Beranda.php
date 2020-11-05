c9878d0ad8cd63cb3729ef4d89912cf36b41f4dd
farhansetyadi <farhan_setyadi@apps.ipb.ac.id>
2020-11-05 17:41:53 +0700

    Pertemuan 6

--- app/app/Controllers/Beranda.php
+++ app/app/Controllers/Beranda.php
@@ -3,7 +3,9 @@ namespace App\Controllers;
 
 class Beranda extends BaseController {
 
-	public function index() {
-		return view('beranda_v');
-	}
+    public function index() {
+        echo view('header_v');
+        echo view('beranda_v');
+        echo view('footer_v');
+    }
 }
